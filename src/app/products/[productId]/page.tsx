2ced30b71c44a7563108ed40fd59c6813f8792a7
Hassan Murtaza <hassanmurtaza.dev@gmail.com>
2024-10-26 20:47:39 +0500

    Lecture 7 - Dynamic Routing

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,8 @@
+export default async function ProductDetails({
+  params,
+}: {
+  params: { productId: string };
+}) {
+  const { productId } = await params;
+  return <h1>Details about product {productId}</h1>;
+}

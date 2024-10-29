2b9ee5f377e284cc144f26dac12f95534643496f
Hassan Murtaza <hassanmurtaza.dev@gmail.com>
2024-10-29 08:58:42 +0500

    Routing Metadata and Title Metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,26 @@
-export default async function ProductDetails({
+import { Metadata } from "next";
+
+export const generateMetadata = async ({
   params,
-}: {
-  params: { productId: string };
-}) {
+}: Props): Promise<Metadata> => {
+  const { productId } = await params;
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${productId}`);
+    }, 1000);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+
+export default async function ProductDetails({ params }: Props) {
   const { productId } = await params;
   return <h1>Details about product {productId}</h1>;
 }
